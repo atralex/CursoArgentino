b4dd453be47a7a3bbaae4088cef59fc2bdd2354f
atralex <atralexrg@gmail.com>
2023-02-22 23:27:24 +0100

    Primer fetch a la API funcionando

---
+++ src/main/resources/static/js/usuarios.js
@@ -0,0 +1,15 @@
+window.addEventListener('DOMContentLoaded', event => {
+    // Simple-DataTables
+    // https://github.com/fiduswriter/Simple-DataTables/wiki
+
+    const usuarios = document.getElementById('usuarios');
+    if (usuarios) {
+        cargarUsuarios();
+        new simpleDatatables.DataTable(usuarios);
+    }
+    async function cargarUsuarios(){
+    fetch('usuario/1234')
+      .then(response => response.json())
+      .then(usuarios => console.log(usuarios));
+    }
+});
