470c20dbf929ae90f51b55a846f6d0b7ec53bf97
atralex <atralexrg@gmail.com>
2023-02-22 23:10:49 +0100

    Primera consumicion de API

--- src/main/java/com/grupodeyonkis/alejandro/curso/controllers/usuarioController.java
+++ src/main/java/com/grupodeyonkis/alejandro/curso/controllers/usuarioController.java
@@ -1,13 +1,47 @@
 package com.grupodeyonkis.alejandro.curso.controllers;
 
+import com.grupodeyonkis.alejandro.curso.models.Usuario;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class usuarioController {
-    @RequestMapping(value ="prueba")
-    public List<String> prueba(){
-        return List.of("manzana", "pera", "mandarina");
+    @RequestMapping(value ="usuario/{id}")
+    public Usuario getUsuario(@PathVariable long id){
+        Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setNombre("Pedro");
+        usuario.setApellido("Picapiedras");
+        usuario.setEmail("pedritopicapleitos@gmail.com");
+        usuario.setPassword("surmano");
+        usuario.setTelefono("655454544");
+
+        return usuario;
+    };
+
+
+    @RequestMapping(value ="usuario2")
+    public Usuario modificaUsuario(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Pedro");
+        usuario.setApellido("Picapiedras");
+        usuario.setEmail("pedritopicapleitos@gmail.com");
+        usuario.setPassword("surmano");
+        usuario.setTelefono("655454544");
+
+        return usuario;
+    };
+
+    @RequestMapping(value ="usuario3")
+    public Usuario listarUsuarios(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Pedro");
+        usuario.setApellido("Picapiedras");
+        usuario.setEmail("pedritopicapleitos@gmail.com");
+        usuario.setPassword("surmano");
+        usuario.setTelefono("655454544");
+
+        return usuario;
     };
 }
